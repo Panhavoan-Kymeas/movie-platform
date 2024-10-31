b92fc45ee923952244f68c5e97cce4c3c926040c
Panhavoan-Kymeas <141219037+Panhavoan-Kymeas@users.noreply.github.com>
2024-10-31 20:07:38 +0700

    added tailwind css

---
+++ app/components/Login.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Login(){
+
+    const loginContainerStyles = {
+        width: '844px',
+        height: '600px',
+        backgroundColor: 'blue',
+    };
+
+    return (
+        <div style={loginContainerStyles}>
+            
+        </div>
+    )
+}
